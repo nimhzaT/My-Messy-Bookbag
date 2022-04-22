95a2e930d95521ac0dc4e940997b21c3f5aa4286
TW_cpyd-mkf <59209802+notechXnolifeXcpyd@users.noreply.github.com>
2022-04-22 16:32:38 -0400

    Add files via upload

---
+++ import tkinter as tk.js
@@ -0,0 +1,23 @@
+import tkinter as tk
+import cgitb
+
+
+def counter_label(label, limit, rate=1):
+    """Update counter display on label at given rate until it reaches limit."""
+    counter = 0
+    def count():
+        nonlocal counter
+        counter += rate
+        label.config(text=str(counter))
+        if counter < limit:
+            label.after(1, count)
+    count()  # Start counting.
+
+root = tk.Tk()
+root.title("Headquarters Count Recycled")
+label= tk.Label(root, fg="dark green")
+label.pack()
+counter_label(label, 8454)
+button = tk.Button(root, text="stop", width=40, command=root.destroy)
+button.pack()
+root.mainloop()
